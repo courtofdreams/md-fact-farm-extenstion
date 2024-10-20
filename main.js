ac03f41889e1d673f3fe4b85fb0ae9cd8818483d
Supakarn B <30888519+courtofdreams@users.noreply.github.com>
2024-10-19 22:33:19 -0700

    feat: add information

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const loading = document.getElementById('loading')
-const quoteElement = document.getElementById('resultElement')
+const headerLogo = document.getElementById('headerLogo')
+const resultElement = document.getElementById('resultElement')
 const authorElement = document.getElementById('reasonElement')
 
 const getQuotes = async (url) => {
+  headerLogo.style.display = 'none'
+  loading.style.display = 'flex'
     if(!url.includes('youtube.com')) {
-      loading.innerHTML = `This is not youtube.com ` + url
+      loading.innerHTML = `This is not youtube.com ` + url;
       return;
     }  
     const ytId = url.replace('https://www.youtube.com/watch?v=', '');
-    loading.style.display = 'block'
     try {
         const res = await fetch(`http://localhost:5100/api/v1/is-fake-news/${ytId}`)
         const data = await res.json()
         loading.style.display = 'none'
-        quoteElement.innerHTML = data.result ? 'This video is fake new' : 'This video is not fake news'
-        authorElement.innerHTML = `${data.reason}`
+        resultElement.innerHTML = data.result;
+        headerLogo.style.display = 'flex'
     } catch (error) {
-        quoteElement.innerHTML = `oops... no qotes to show`
+        loading.style.display = 'none'
+        headerLogo.style.display = 'flex'
+        resultElement.innerHTML = `oops... no result to show`
     }
 
 }
@@ -24,6 +28,7 @@ const getQuotes = async (url) => {
 
 // see the note below on how to choose currentWindow or lastFocusedWindow
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+  headerLogo.style.display = 'none'
   let url = tabs[0].url;
   getQuotes(url)
   // use `url` here inside the callback because it's asynchronous!
